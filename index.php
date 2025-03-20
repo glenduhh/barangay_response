24f2c840e74e14be69dd6dded9c149ad1ce11f76
Glenda Galura <glenda.galura8@gmail.com>
2025-03-20 10:45:32 +0800

    mysql

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
 header("Location: index.html");
+
+$uri = "mysql://avnadmin:AVNS_AiJNBy4HrV8__tFKsKE@admin-login-barangay-response.l.aivencloud.com:24093/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=defaultdb";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try {
+  $db = new PDO($conn, $fields["user"], $fields["pass"]);
+
+  $stmt = $db->query("SELECT VERSION()");
+  print($stmt->fetch()[0]);
+} catch (Exception $e) {
+  echo "Error: " . $e->getMessage();
+}
 exit();
 ?>
